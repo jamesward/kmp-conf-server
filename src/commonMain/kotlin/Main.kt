8825ee8542eb24375f275937c1180512fb7d4c50
James Ward <james@jamesward.com>
2023-09-13 14:17:58 -0600

    more sessions

--- src/commonMain/kotlin/Main.kt
+++ src/commonMain/kotlin/Main.kt
@@ -19,9 +19,30 @@ val sessions = listOf(
         389291,
         "Kotlin Multiplatform Conversions at Android Jetpack Scale",
         """
-                This is a case study of how we converted several Jetpack libraries to Kotlin Multiplatform as part of our ongoing experimentation with the multiplatform technology.
-            """.trimIndent()
+            This is a case study of how we converted several Jetpack libraries to Kotlin Multiplatform as part of our ongoing experimentation with the multiplatform technology.
+        """.trimIndent()
     ),
+    Session(
+        405587,
+        "Compose Multiplatform on iOS",
+        """
+            An overview of the exciting present and future of Compose Multiplatform, including live demos and how to get started with the latest additions to the multiplatform UI framework built by JetBrains.
+        """.trimIndent()
+    ),
+    Session(
+        388863,
+        "Level up on Kotlin Multiplatform",
+        """
+            Kotlin Multiplatform is a new cross-platform technology from JetBrains that is taking the mobile world by storm. Many Android and iOS developers have given this exciting technology a try. But how do you take your skills from building simple apps to more complex ones?
+        """.trimIndent()
+    ),
+    Session(
+        391895,
+        "Kotlin Multiplatform Mobile for Teams",
+        """
+            Touchlab has spent the past few years integrating KMM into various apps. The most simple observation we've taken away from that experience is that different teams approach KMM in different ways. What works well for small teams may be different than for larger teams. What works well for greenfield apps may be different than for existing app projects. In this session, we'll cover these different integration approaches. We'll also discuss tooling options, iOS-side API design tips, developer workflows with shared code, and some thoughts on how to present KMM to a team that may not know it needs it (yet!)
+        """.trimIndent()
+    )
 )
 
 fun main() = SuspendApp {
